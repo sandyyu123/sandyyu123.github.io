de98e30a005b6f466439d198b3fb39bf442221ae
sandyyu123 <sandyyu2028@gmail.com>
2023-12-22 20:40:23 +0800

    a

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig({
 
   lang: "en-US",
   title: "Mr.Hope's Personal Website",
-  description: "A blog demo for vuepress-theme-hope",
+  description: "Welcome to my website, I'm Mr. Hope. Here, you'll find my notes and blogs on various topics. I'm into coding, swimming, and hitting the beach. Explore with me as I share insights on coding and bits of my beachside adventures. Hope you find something cool here!",
 
   theme,
 
