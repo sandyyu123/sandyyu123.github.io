03e1be37470f5992c8f2d48f441b8193617feb8d
sandyyu123 <sandyyu2028@gmail.com>
2024-01-05 20:00:00 +0800

    a

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,9 @@
+import {defineClientConfig} from "@vuepress/client";
+import Particles from "./components/Particles/Particles.vue";
+
+export default defineClientConfig({
+    enhance({app, router, siteData}) {},
+    setup() {},
+    layouts: {},
+    rootComponents: [Particles],
+})
---
+++ src/.vuepress/components/Particles/Particles.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { onMounted } from "vue";
+async function loadDependancies() {
+
+    await import("./lib/EasePack.min.js");
+    await import("./lib/TweenLite.min.js");
+    await import("./lib/particles.js");
+}
+onMounted(loadDependancies);
+</script>
+<template>
+    <div id="particles-wrapper">
+        <canvas id="particles-canvas"></canvas>
+    </div>
+</template>
+
+<style lang="less">
+#particles-wrapper {
+    z-index: 1;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-size: cover;
+    background-position: center center;
+    // background-color: #000000;  // 黑色
+    background-color: transparent; // 透明背景
+}
+</style>
