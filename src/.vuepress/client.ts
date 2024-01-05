4a3efd2fbad2d87220d8a3ee6c1d628439f20841
sandyyu123 <sandyyu2028@gmail.com>
2024-01-05 20:34:14 +0800

    aaaaaaaa

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,9 +1,11 @@
-import {defineClientConfig} from "@vuepress/client";
+import { defineClientConfig } from "@vuepress/client";
 import Particles from "./components/Particles/Particles.vue";
 
 export default defineClientConfig({
-    enhance({app, router, siteData}) {},
+    enhance({ app, router, siteData }) {},
     setup() {},
     layouts: {},
     rootComponents: [Particles],
 })
+
+// pnpm add -D less
